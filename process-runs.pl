54fa48c373f32aa0ca668bd129bc533c0b79f2d1
Karl Kosack <karl.kosack@cea.fr>
2009-12-07 11:25:32 +0100

    turned on "use strict" to avoid bugs

--- process-runs.pl
+++ process-runs.pl
@@ -1,62 +1,73 @@
 #!/usr/bin/perl
 
 use File::Basename;
+use strict 'vars';
 
+my $Dryrun=0;
+my $CenterRA = 83.633333;
+my $CenterDec= 22.014444;
+my $GeomX = 256;
+my $GeomY = 256;
+my $FOVX = 6.0;
+my $FOVY = 6.0;
 
-$Dryrun=0;
-$CenterRA = 83.633333;
-$CenterDec= 22.014444;
-$GeomX = 256;
-$GeomY = 256;
-$FOVX = 6.0;
-$FOVY = 6.0;
+my $indir= "$ENV{HOME}/Analysis/FITSEventLists/HESS_Crab";
+my $outdir = "$ENV{HOME}/Analysis/FITSEventLists/Analysis";
+my @infilelist = <$indir/*.fits.gz>; # list of input eventlist files
 
-$outdir = "$ENV{HOME}/Analysis/FITSEventLists/Analysis";
-
+my %sum;
 $sum{cmap} = "$outdir/sum_cmap.fits";
 $sum{cmap_masked} = "$outdir/sum_cmap_masked.fits";
 $sum{acc} = "$outdir/sum_acc.fits";
 $sum{fov_excess} = "$outdir/sum_fov_excess.fits";
 
-foreach $map (keys %sum) {
+foreach my $map (keys %sum) {
     print "Clearing sum: $map\n";
     unlink $sum{$map};
 }
 
-$PYTHON="python2.6";
+my $PYTHON="python2.6";
 if (system("python2.6 -V")){
     $PYTHON="python2.4";
 }
 
-$is_first_iter = 1;
-
+my $is_first_iter = 1;
 
 MAIN: {
 
-    foreach $evlist (<$ENV{HOME}/Analysis/FITSEventLists/HESS_Crab/*.fits.gz>) {
+    foreach my $evlist (@infilelist) {
 
-	$bname=basename($evlist,"_eventlist.fits.gz");
-	$bname="$outdir/$bname";
+	my $run=0;
+	$_ = $evlist; 
+	$run = $1+0 if (/.*run_([0-9]+).*/);
+	print "RUN: $run\n";
+	
+	my $subdir = sprintf("run%06d", $run );
+
+	my $bname=basename($evlist,"_eventlist.fits.gz");
+	$bname="$outdir/$subdir/$bname";
+	mkdir("$outdir/$subdir") if (! -e "$outdir/$subdir");
 
 	print "="x70,"\n";
 	print " $evlist \n";
 	print "="x70,"\n";
 
 	# apply gamma-hadron cuts
-	$selname = "${bname}_event_selected.fits.gz";
-	$inname="${evlist}[EVENTS]";
-	$cuts = 'HIL_MSW > -2.0 && HIL_MSW < 0.7 && HIL_MSL > -2.0 && HIL_MSL < 2.0';
+	my $selname = "${bname}_event_selected.fits.gz";
+	my $inname="${evlist}[EVENTS]";
+	my $cuts = '(HIL_MSW > -2.0 && HIL_MSW < 0.7) ' 
+	    . '&& (HIL_MSL > -2.0 && HIL_MSL < 2.0)';
 
 	if (! -e $selname ) {
 	    print "SELECT: --> $selname\n";
 	    runtool( "ftselect $inname $selname '$cuts'");
 	}
 
-	# apply sourcemask cuts (not sure this is the best way to do this,
-	# but it works)
+	# apply spatial cuts, masking out known sources, and generate
+	# a "masked" eventlist (useful for acceptance calculation)
 
-	$maskname = "${bname}_event_masked.fits.gz";
-	$exclmask = "regfilter(\"excluded.reg\",RA,DEC)";
+	my $maskname = "${bname}_event_masked.fits.gz";
+	my $exclmask = "regfilter(\"excluded.reg\",RA,DEC)";
 	if (! -e $maskname) {
 	    print "EXCLUDE: --> $maskname \n";
 	    runtool( "ftselect $selname $maskname '$exclmask'")
@@ -64,17 +75,19 @@ MAIN: {
 
 	# make a countmap
 
-	$cmapname="${bname}_cmap.fits";
-	$maskcmapname="${bname}_cmap_masked.fits";
+	my $cmapname="${bname}_cmap.fits";
+	my $maskcmapname="${bname}_cmap_masked.fits";
 
 	makeMap( $cmapname, $selname );
 	makeMap( $maskcmapname, $maskname );
 
-	# generate exclusion map
-	$flatlistname ="$outdir/flatlist.fits";
-	$excllistname ="$outdir/excllist.fits";
-	$flatmapname = "$outdir/flatmap.fits";
-	$exclmapname = "$outdir/exclmap.fits";
+	# generate exclusion map, which is just a binned version of
+	# the exclusion mask. This can be used for example to
+	# calcualate the area covered by masked regions
+	my $flatlistname ="$outdir/flatlist.fits";
+	my $excllistname ="$outdir/excllist.fits";
+	my $flatmapname = "$outdir/flatmap.fits";
+	my $exclmapname = "$outdir/exclmap.fits";
 	if ($is_first_iter) {
 	    unlink $flatlistname;
 	    unlink $excllistname;
@@ -88,23 +101,26 @@ MAIN: {
 	    makeMap( $exclmapname, $excllistname );
 	}
 
-	# generate acceptance map
+	# generate acceptance map from the data:
 
-	$accname = "${bname}_acc.fits";
+	my $accname = "${bname}_acc.fits";
 	if (! -e $accname ){ 
-	    runtool( "$PYTHON acceptance.py --output $accname $maskname $cmapname" );
+	    runtool( "$PYTHON acceptance.py --output $accname ".
+		     "$maskname $cmapname" );
 	}
 
-	# generate runwise FOV-model excess map
-	$excessname = "${bname}_fov_excess.fits";
-	$excessnamemasked = "${bname}_fov_excess_masked.fits";
+	# generate runwise FOV-model excess map (uses the acceptance
+	# map as the background map)
+	my $excessname = "${bname}_fov_excess.fits";
+	my $excessnamemasked = "${bname}_fov_excess_masked.fits";
 	if (! -e $excessname  ) {
 	    print "FOV EXCESS: $excessname\n";
 	    runtool( "ftpixcalc $excessname 'A-B' a=$cmapname b=$accname" );
-	    runtool( "ftpixcalc $excessnamemasked 'A-B' a=$maskcmapname b=$accname" );
+	    runtool( "ftpixcalc $excessnamemasked 'A-B' ".
+		     "a=$maskcmapname b=$accname" );
 	}
 
-	# update the summed countmap
+	# update the maps that should be summed over runs:
 
 	updateSum( $sum{cmap}, $cmapname );
 	updateSum( $sum{cmap_masked}, $maskcmapname );
@@ -116,7 +132,7 @@ MAIN: {
     }
 
     # make fov excess map from sums:
-    $fovexcessname = "$outdir/excess_fov.fits";
+    my $fovexcessname = "$outdir/excess_fov.fits";
     print "FOV EXCESS MAP: ";
     runtool("ftpixcalc $fovexcessname 'A-B' a=$sum{cmap} b=$sum{acc} clobber=true");
 
@@ -125,7 +141,8 @@ MAIN: {
 sub makeMap($$) {
     my $mapname=shift;
     my $eventlist=shift;
-    my $args="--fov $FOVX,$FOVY --geom $GeomX,$GeomY --center $CenterRA,$CenterDec";
+    my $args="--fov $FOVX,$FOVY --geom $GeomX,$GeomY ".
+	"--center $CenterRA,$CenterDec";
      
     if (! -e $mapname ) {
 	print "CMAP: --> $mapname\n";
