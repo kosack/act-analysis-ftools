5bc87a264b83b7d8f246c8bc8d251af58d7f7a33
Karl Kosack <karl.kosack@cea.fr>
2010-03-03 16:56:35 +0100

    added getValue function

--- fitshistogram.py
+++ fitshistogram.py
@@ -5,7 +5,7 @@ import math
 from kapteyn import wcs,maputils
 
 
-class FITSHistogram(object):
+class Histogram(object):
     """
     A simple N-dimensional histogram class with FITS IO 
 
@@ -14,23 +14,26 @@ class FITSHistogram(object):
     """
     
     
-    def __init__(self, bins=(0), ranges=[[0,0]], name=None):
+    def __init__(self, bins=None, range=None, name=None,
+                 axisTypes=None, axisNames=None):
         """ Initialize an unfilled histogram (need to call either
         fill() or loadFromFITS() to put data into it)
 
         `bins`: array listing binsize for each dimension
         `ranges`: array of ranges for each dimension
-        `name`: optional name/title of the histogram 
+        `name`: name (use for FITS extension name) 
         """
         
-        self.hist = None
-        self.binLowerEdges= None
-        self.bins = bins
-        self.ranges=ranges
+        self.hist = np.zeros(bins)
+        self._binLowerEdges= None  #TODO: should be a property, get only
+        self._bins = bins
+        self._range=range
         self.valueScale = None
-        self.axisTypes=None
+        self.valueZero = None
+        self.axisTypes=axisTypes
+        self.axisNames=axisNames
         self.name = name
-
+        self._proj = None
 
     def fill(self, datapoints, **kwargs):
         """
@@ -41,18 +44,16 @@ class FITSHistogram(object):
         """
 
         hist, binLowerEdges = np.histogramdd( datapoints, 
-                                              bins=self.bins, 
-                                              range=self.ranges, *kwargs)
+                                              bins=self._bins, 
+                                              range=self._range, **kwargs)
         
-        if (self.hist != None and any(binLowerEdges==self.binLowerEdges) ==false):
+        if (self._binLowerEdges!=None 
+            and any(binLowerEdges==self._binLowerEdges)==false):
             raise exceptions.ArithmaticError("Bad Geometry!")
         
-        if (self.hist == None):
-            self.hist = hist
-        else:
-            self.hist += hist
-
-        self.binLowerEdges = binLowerEdges
+        
+        self.hist += hist
+        self._binLowerEdges = binLowerEdges
         
 
 
@@ -66,14 +67,30 @@ class FITSHistogram(object):
 
         """
         ohdu = pyfits.ImageHDU(data=self.hist.transpose())
-        ndim = len(self.bins)
+        ohdu.name = self.name
+        ndim = len(self._bins)
+
+        # the lower-left edge of the first bin is (Xed[0],Yed[0]), which
+        # is (0.5,0.5) in FITS pixel coordinates (the center of the bin is
+        # at (1.0,1.0))
+
+
+        # now note that this defines the first pixel in FITS coordinates
+        # with the center (1.0,1.0). in integer python coordinates it is [0,0]
+
+        # to transform a world value, you need to subtract 1.0 and round
+        # down to get the bin number:
+        #       ibin = round( Vpix-1.0 )
+        # To get the value of ibin, you need to go the other way:
+        #       Vworld[ibin] = proj.toworld( ibin+0.5 )
+        
 
         for dim in xrange(ndim):
-            width = self.ranges[dim][1] - self.ranges[dim][0]
-            num = self.bins[dim]
+            width = self._range[dim][1] - self._range[dim][0]
+            num = self._bins[dim]
             delta = width/float(num)
             bin0pix = 0.5 # lower-left corner of first bin
-            bin0coord = self.ranges[dim][0]
+            bin0coord = self._range[dim][0]
 
             ohdu.header.update("CTYPE%d"%(dim+1),"LIN","Linear parameter") 
             ohdu.header.update("CDELT%d"%(dim+1), delta)
@@ -82,6 +99,10 @@ class FITSHistogram(object):
             
         if (self.valueScale):
             ohdu.header.update( "BSCALE", 1.0/float(self.valueScale) )
+
+        if (self.valueZero):
+            ohdu.header.update( "BZERO", float(self.valueZero) )
+
    
         return ohdu
 
@@ -100,22 +121,67 @@ class FITSHistogram(object):
             hdu = inputFITS
             
         self.hist = hdu.data.transpose()
-        self.bins = self.hist.shape
+        self._bins = self.hist.shape
 
         proj = wcs.Projection ( hdu.header )
-        ndim = len(self.bins)
+        ndim = len(self._bins)
         
         edges = []
+        self._range = []
 
         for dim in xrange(ndim):
-            a = np.zeros( (self.bins[dim], ndim ) )
-            a[:,dim] = np.arange( self.bins[dim] )+0.5
+            # note that histogramdd returns edges for 0-N+1 (including
+            # the lower edge of the non-existant next bin), so we do
+            # the same here to keep things the same
+            a = np.zeros( (self._bins[dim]+1, ndim ) )
+            a[:,dim] = np.arange( self._bins[dim]+1 )+0.5
             edges.append(proj.toworld( a )[:,dim])
+            self._range.append( (edges[dim][0],edges[dim][-1]) )
             
-        self.binLowerEdges =  edges 
+        self._binLowerEdges = edges 
+        self._range = np.array(self._range)
+        
+        if(hdu.header.get("BSCALE")):
+            self.valueScale = hdu.header["BSCALE"]
+
+        if(hdu.header.get("BSCALE")):
+            self.valueZero = hdu.header["BZERO"]
+    
+
+    def getProjection(self ):
+        """
+        """
+        
+        if (self._proj == None):
+            self._proj = wcs.Projection( self.asFITS().header )
+
+        return self._proj
 
         
-
-
+    def getValue(self, coordinate, outlierValue=None):
+        """ Returns the value of the histogram at the given world
+        coordinate(s) 
         
+        Arguments:
+
+        - `coordinate`: single coordinate or array-like list of
+          coordinates (in world coordinates)
+        """
         
+        # TODO: this can be sped up a lot probably! No need for the
+        # for loop if we're smart... see how it's done in kapteyn?
+
+        world = np.array( coordinate )
+        bins = np.trunc(self.getProjection().topixel( world ) - 1.0).astype(int)
+        maxbin = np.array(self.hist.shape)
+
+        val = np.zeros(len(world))
+
+        for ii,bin in enumerate(bins):
+            outlier = ((bin>=maxbin) * (bin <0)).any()
+            if (outlier):
+                val[ii] = outlierValue
+            else:
+                val[ii] = self.hist[tuple(bin)]
+    
+        return val
