4bd631b3c1371a9505dd761de50a0e439b1582ee
Karl Kosack <karl.kosack@cea.fr>
2010-04-26 11:17:29 +0200

    really removed debug output

--- sum-maps.py
+++ sum-maps.py
@@ -26,8 +26,6 @@ bunchsize = len(alpha)
 bunches = np.array_split( args, floor(len(args)/bunchsize)+1 )
 tmpname = "temp-"+opts.output
 
-if opts.verbose: print args
-
 tot = len(args) # first file is already summed
 count = 0
 
@@ -44,7 +42,6 @@ for bunch in bunches:
         vals += " %s=%s" % (var,fname)
         
     cmd = "ftpixcalc %s '%s' %s" % (tmpname,expr,vals)
-    if  opts.verbose: print "DEBUG: ",cmd
     os.system(cmd)
     os.rename( tmpname, opts.output )
         
