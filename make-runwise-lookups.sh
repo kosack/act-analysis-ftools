403fc3e19b86d209fb8346ff9deeb9d4ea97ba9a
Karl Kosack <karl.kosack@cea.fr>
2010-10-29 16:35:44 +0200

    changed input dir

--- make-runwise-lookups.sh
+++ make-runwise-lookups.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for ii in ~/Data/FITS/HESS_Simulations/*.fits.gz; do
+for ii in ~/Data/FITS/HESS/Simulations/Phase1b/Gamma/0.7deg/*.fits.gz; do
     python ${TOOLSDIR}/generate-lookup-tables.py $ii
 done
 
