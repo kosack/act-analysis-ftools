1905e9a6c469af15075ef0c48bcb5b51f80ace37
Karl Kosack <karl.kosack@cea.fr>
2010-11-16 15:25:28 +0100

    added documentation and fixed bug in eventlist-to-jboost.py

--- eventlist-to-jboost.py
+++ eventlist-to-jboost.py
@@ -12,7 +12,7 @@ training dataset
 """
 
 
-def generateSpecFile(hdu,cols,outputfile="training.spec"):
+def generateSpecFile(hdu,cols, outputfile="training.spec"):
     """
     Generate a JBoost spec file from the columns in a fits data table
     
@@ -26,9 +26,27 @@ def generateSpecFile(hdu,cols,outputfile="training.spec"):
     with open(outputfile, mode='w') as out:
         out.write("exampleTerminator=;\n")
         out.write("attributeTerminator=,\n")
-        
+        out.write("labels (gamma,hadron)\n")
         for col in cols:
             out.write("{0:20s} number\n".format(col) )
+
+def dumpEvents(events,cols,label,outputstream ):
+    """
+    dump the specified columns to a comma-separated list
+
+    Arguments:
+    - `events`: a BINTABLE hdu
+    - `cols`: list of columns to use for training
+    """
+    
+    data = zip( *[events.data.field(trainingColumns[ii]) 
+                  for ii in xrange(len(trainingColumns))] )
+    
+    for ii in xrange(len(data)):
+        out.write(label+",")
+        outputstream.write(",".join(["{0}".format(val) for val in data[ii]]))
+        outputstream.write(";\n")
+
 
 
 if __name__ == '__main__':
@@ -37,33 +55,34 @@ if __name__ == '__main__':
     parser.set_usage("eventlist-to-jboost.py [options] <eventlist> [<eventlist ..]")
     parser.add_option( "-o","--output", dest="output",  default="training", 
                        help="base output filename")    
+    parser.add_option( "-l","--label", dest="label",  default="gamma", 
+                       help="whether this is gamma or hadron")    
     (opts,args) = parser.parse_args()
     if len(args)==0:
         parser.error("specify eventlist filename(s)")
 
-    trainingColumns = ["HIL_MSW","HIL_MSL"]
+    trainingColumns = ["HIL_MSW","HIL_MSL","XMAX","XMAX_ERR"]
+    trainfile = opts.output+".dat"
+    print "Output to:",trainfile
 
     first = True
 
-    with open(opts.output+".train",mode="w") as out:
+    with open(trainfile, mode="w") as out:
 
         for ii,eventlist_filename in enumerate(args):
 
-            print "[{0:3d}/{1:3d}] ".format(ii,len(args)), eventlist_filename
-
             evfile = pyfits.open(eventlist_filename)
             events = evfile["EVENTS"]
-        
+
+            print "[{0:3d}/{1:3d}] {2:5d}".format(ii,len(args),len(events.data)), eventlist_filename
+
+
+
             if (first):
-                generateSpecFile( hdu=events, cols=trainingColumns,
+                generateSpecFile( hdu=events, cols=trainingColumns, 
                                   outputfile=opts.output+".spec")
                 first=False
-                
-            data = zip( *[events.data.field(trainingColumns[ii]) 
-                          for ii in xrange(len(trainingColumns))] )
-            
-            for ii in xrange(len(data)):
-                out.write(",".join(["{0}".format(val) for val in data[ii]]))
-                out.write("\n")
-                          
+
+            dumpEvents( events, cols=trainingColumns, outputstream=out,                                  label=opts.label)
+
 
