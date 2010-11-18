379002427701a9c3f25807705045879c401453e4
Karl Kosack <karl.kosack@cea.fr>
2010-11-18 14:17:36 +0100

    dump out python code too, display Q factor in separation plot

--- GammaHadronSeparation/BDT/showseparation.py
+++ GammaHadronSeparation/BDT/showseparation.py
@@ -4,12 +4,15 @@ protons = loadtxt("classify_protons.dat")
 gammas = loadtxt("classify_gammas.dat");
 
 hrange = [min(protons.min(), gammas.min())-1.0,max(protons.max(), gammas.max())+1.0]
-hbins = 20
+hbins = 30
 
 pbins,edges = np.histogram(protons,bins=hbins, range=hrange, normed=True)
 gbins,edges = np.histogram(gammas,bins=hbins, range=hrange, normed=True)
 
+pbins /= pbins.sum()
+gbins /= gbins.sum()
 
+subplot(2,1,1)
 title("Gamma-Hadron sepration power")
 plot( edges[0:-1], pbins, drawstyle='steps-pre', color='r', label="Protons",lw=2 )
 plot( edges[0:-1], gbins, drawstyle='steps-pre', color='g', label="Gammas",lw=2 )
@@ -17,4 +20,24 @@ xlabel("$\zeta$")
 ylabel("probability");
 legend(loc="upper left")
 
+
+
+
+
+eff_g = np.zeros_like(pbins)
+eff_p = np.zeros_like(pbins)
+
+ii = arange(len(pbins))
+
+for bin in xrange(len(pbins)):
+    eff_g[bin] = gbins[bin:].sum()
+    eff_p[bin] = pbins[bin:].sum()
+
+qfact = eff_g/sqrt(eff_p)
+
+
+subplot(2,1,2)
+plot( edges[0:-1], qfact, drawstyle="steps-pre" )
+xlabel("$\zeta$")
+ylabel("Q")
 savefig("separation.pdf")
