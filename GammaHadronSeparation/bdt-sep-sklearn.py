5d552950ebf7ea114fff5e8cf252aefdb3c22c18
Karl Kosack <karl.kosack@cea.fr>
2012-10-18 12:48:27 +0200

    updated docs

--- GammaHadronSeparation/bdt-sep-sklearn.py
+++ GammaHadronSeparation/bdt-sep-sklearn.py
@@ -9,7 +9,18 @@ DATADIR=os.path.expanduser("~kosack/Data/FITS/HESS/Simulations/Phase1b")
 
 
 def loadData( filepattern, maxevents=50000 ):
-    """ returns array of MSW values for given data """
+    """ 
+    returns array of MSW values for given data
+
+    - filepattern: shell pattern for file(s) to open (e.g. proton-*.fits)
+      . If it matches more than one file, all files will be opened and
+      the result concatinated.
+
+    - maxevents: keep loading data from files in the filepattern until
+      this number of events is reached (discarding everything
+      afterward)
+
+    """
 
     print filepattern
     msw = np.array( [] )
@@ -31,11 +42,11 @@ def loadData( filepattern, maxevents=50000 ):
 
     return msw
     
+#======================================================================
 
-
-gmsw = loadData(DATADIR+"/Gamma/0.7deg/run_*_eventlist.fits.gz",maxevents=10000)
+gmsw = loadData(DATADIR+"/Gamma/0.7deg/run_*_eventlist.fits.gz",
+                maxevents=10000)
 pmsw = loadData(DATADIR+"/Proton/run_*.fits.gz",maxevents=30000)
-
 
 gtype = ones_like(gmsw) # gammas are labeled 1
 ptype = zeros_like(pmsw) # protons are labeled 0
@@ -44,7 +55,7 @@ x= np.concatenate([gmsw,pmsw])[newaxis].T # the array of gamma, proton values
 y= np.concatenate([gtype,ptype]) # the array of gamma, proton labels
 
 print "Training..."
-cl = tree.DecisionTreeClassifier(max_depth=4)
+cl = tree.DecisionTreeClassifier(max_depth=6)
 cl.fit(x,y)
 print "done."
 
@@ -58,7 +69,7 @@ phist,xe,pat = hist(pmsw, range=[-3,10], bins=100, label="Protons")
 
 # generate some test values to plot:
 
-testx = linspace(-3,10,100)[newaxis].T
+testx = linspace(-3,10,200)[newaxis].T
 testy = cl.predict(testx) # predicts the class (1 or 0)
 
 plot( testx,testy*ghist.max(), drawstyle='steps', lw=3,label="Class" )
@@ -78,3 +89,4 @@ legend()
 outfile = tree.export_graphviz(cl,"test.dot")
 outfile.close()
 
+show()
